56b2793688964be4d5c489741c92b4a1f0e0129a
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-06 22:16:16 +0800

    init: configure eslint (airbnb)

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
     'plugin:prettier/recommended',
   ],
   root: true,
