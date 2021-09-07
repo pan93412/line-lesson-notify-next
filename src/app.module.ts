3100d7f9b2ad6b72441c10645f662d93ed8fe474
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-07 17:23:41 +0800

    feat(app/module): allow to configure with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminPanelModule } from './admin-panel/admin-panel.module';
 import { StatusImageModule } from './status-image/status-image.module';
+import { LineNotifyModule } from './line-notify/line-notify.module';
 
 @Module({
-  imports: [AdminPanelModule, StatusImageModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AdminPanelModule,
+    StatusImageModule,
+    LineNotifyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
