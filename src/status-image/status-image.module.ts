076c58e82a847b6d0fdcf50a1ba53ee368974f72
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-10 02:32:28 +0800

    refactor!: remove status-image module since it has been deprecated

--- src/status-image/status-image.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StatusImageService } from './status-image.service';
-
-@Module({
-  providers: [StatusImageService],
-  exports: [StatusImageService],
-})
-export class StatusImageModule {}
